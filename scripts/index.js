0b2470d29d49b8316a356080283b7b45e8d22a30
Glowrare <aajibolanle@yahoo.com>
2021-05-23 22:14:45 +0100

    About page updated and minor styling update

--- scripts/index.js
+++ scripts/index.js
@@ -18,8 +18,9 @@ function aboutPage() {
     <div>
       <p>
         Function Simulator Project is an open source project created by Ajibolanle Gloria to write custom vanilla JS function logic to mimic diverse JS methods. <br />
+        
         <br />
-        The primary aim of this exercise is to practice logic reasoning and basic DOM manipulation. <br />
+        The primary aim of this exercise is to practice logic reasoning and basic DOM manipulation. The functions can also be adapted for diverse related use in individual projects. <br />
         <br />
         The project is open source so collaboration and suggestions are welcome for improvement and to create a robust function base.
       </p>
